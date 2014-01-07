fb983769cde26d526c834e7c8069020d989ca66e
James T. Allen <j.allen@physics.usyd.edu.au>
2014-01-07 20:17:12 +1100

    Raise 404 on request for telescope that doesn't exist.

--- observations/views.py
+++ observations/views.py
@@ -2,12 +2,14 @@ import json
 from datetime import date, timedelta
 from calendar import monthrange
 
+from django.http import Http404
 from django.shortcuts import render
 
 from observations.models import Telescope, Instrument, Night, Exposure
 
 def night(request, name, year, month, day):
     """A specific night on a specific telescope."""
+    check_telescope_name(name)
     date_obs = date(int(year), int(month), int(day))
     night_list = Night.objects.filter(
         instrument__telescope__name=name, ut_date=date_obs)
@@ -15,18 +17,21 @@ def night(request, name, year, month, day):
 
 def month(request, name, year, month):
     """A specific month on a specific telescope."""
+    check_telescope_name(name)
     first_date = date(int(year), int(month), 1)
     last_date = date(int(year), int(month), monthrange(int(year), int(month))[1])
     return render_night_range(request, first_date, last_date, name)
 
 def year(request, name, year):
     """A specific year on a specific telescope."""
+    check_telescope_name(name)
     first_date = date(int(year), 1, 1)
     last_date = date(int(year), 12, 31)
     return render_night_range(request, first_date, last_date, name)
 
 def telescope(request, name):
     """All observations on a specific telescope."""
+    check_telescope_name(name)
     night_list = Night.objects.filter(
         instrument__telescope__name=name)
     return render_night_list(request, night_list)
@@ -55,3 +60,14 @@ def render_night_list(request, night_list):
     return render(request, 'observations/observations.html',
                   {'instrument_name_set': instrument_name_set,
                    'exposure_list': exposure_json})
+
+def check_telescope_name(name):
+    """
+    Check that a telescope exists, and raise a 404 if it doesn't. Returns
+    True if the telescope does exist.
+    """
+    try:
+        Telescope.objects.get(name=name)
+    except Telescope.DoesNotExist:
+        raise Http404
+    return True
