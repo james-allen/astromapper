70164a8969e0a86a6e219ce8acb09e938dfa4d0b
James T. Allen <j.allen@physics.usyd.edu.au>
2014-01-09 07:21:54 +1100

    Added extra tests for telescope views.

--- observations/tests.py
+++ observations/tests.py
@@ -56,13 +56,103 @@ class ObservationsViewTests(TestCase):
         """
         Test which exposures are returned when a telescope is selected.
         """
-        populate_database('super_big_telescope', 'awesome_instrument')
-        populate_database('really_big_telescope', 'excellent_instrument')
+        telescope_name = 'super_big_telescope'
+        instrument_name = 'awesome_instrument'
+        create_exposures(telescope_name, instrument_name, 2012, 6, 1)
+        create_exposures(telescope_name, instrument_name, 2013, 7, 1)
+        expected_exposures = [
+            '<Exposure: %s 2012/06/01 run 1>' % telescope_name,
+            '<Exposure: %s 2012/06/01 run 2>' % telescope_name,
+            '<Exposure: %s 2013/07/01 run 1>' % telescope_name,
+            '<Exposure: %s 2013/07/01 run 2>' % telescope_name,
+            ]
         response = self.client.get(reverse(
             'observations:telescope', args=('super_big_telescope',)))
-        exposure_list = expected_exposure_list('super_big_telescope')
         self.assertQuerysetEqual(response.context['exposure_list'], 
-                                 exposure_list)
+                                 expected_exposures)
+
+    def test_view_telescope_year(self):
+        """
+        Test which exposures are returned when a telescope and year are
+        selected.
+        """
+        telescope_name = 'super_big_telescope'
+        instrument_name = 'awesome_instrument'
+        create_exposures(telescope_name, instrument_name, 2012, 12, 31)
+        create_exposures(telescope_name, instrument_name, 2013, 1, 1)
+        create_exposures(telescope_name, instrument_name, 2013, 12, 31)
+        create_exposures(telescope_name, instrument_name, 2014, 1, 1)
+        expected_exposures = [
+            '<Exposure: %s 2013/01/01 run 1>' % telescope_name,
+            '<Exposure: %s 2013/01/01 run 2>' % telescope_name,
+            '<Exposure: %s 2013/12/31 run 1>' % telescope_name,
+            '<Exposure: %s 2013/12/31 run 2>' % telescope_name,
+            ]
+        response = self.client.get(reverse(
+            'observations:year', args=('super_big_telescope','2013')))
+        self.assertQuerysetEqual(response.context['exposure_list'], 
+                                 expected_exposures)
+
+    def test_view_telescope_month(self):
+        """
+        Test which exposures are returned when a telescope and month are
+        selected.
+        """
+        telescope_name = 'super_big_telescope'
+        instrument_name = 'awesome_instrument'
+        create_exposures(telescope_name, instrument_name, 2013, 3, 31)
+        create_exposures(telescope_name, instrument_name, 2013, 4, 1)
+        create_exposures(telescope_name, instrument_name, 2013, 4, 30)
+        create_exposures(telescope_name, instrument_name, 2013, 5, 1)
+        expected_exposures = [
+            '<Exposure: %s 2013/04/01 run 1>' % telescope_name,
+            '<Exposure: %s 2013/04/01 run 2>' % telescope_name,
+            '<Exposure: %s 2013/04/30 run 1>' % telescope_name,
+            '<Exposure: %s 2013/04/30 run 2>' % telescope_name,
+            ]
+        response = self.client.get(reverse(
+            'observations:month', args=('super_big_telescope','2013','4')))
+        self.assertQuerysetEqual(response.context['exposure_list'], 
+                                 expected_exposures)
+
+def create_exposures(telescope_name, instrument_name, year_int, month_int, 
+                     night_int):
+    """
+    Put two object exposures and one calibration exposure on the given
+    night for the specified telescope/instrument.
+    """
+    # Use an existing telescope if available, or create it
+    try:
+        telescope = Telescope.objects.get(name=telescope_name)
+    except Telescope.DoesNotExist:
+        telescope = Telescope.objects.create(
+            name=telescope_name, latitude=25.0, longitude=45.0)
+    # Use an existing instrument if available, or create it
+    try:
+        instrument = Instrument.objects.get(
+            name=instrument_name, telescope=telescope)
+    except Instrument.DoesNotExist:
+        instrument = Instrument.objects.create(
+            name=instrument_name, telescope=telescope)
+    # Use an existing night if available, or create it
+    ut_date = date(year_int, month_int, night_int)
+    try:
+        night = Night.objects.get(
+            instrument=instrument, ut_date=ut_date)
+    except Night.DoesNotExist:
+        night = Night.objects.create(
+            instrument=instrument, ut_date=ut_date)
+    # Make the three exposures
+    Exposure.objects.create(
+        night=night, run_number=1, ut_start=time(10, 0, 0),
+        exposed=20.0, ra=60.0, dec=30.0, object_exp=True)
+    Exposure.objects.create(
+        night=night, run_number=2, ut_start=time(11, 0, 0),
+        exposed=30.0, ra=90.0, dec=0.0, object_exp=True)
+    Exposure.objects.create(
+        night=night, run_number=3, ut_start=time(12, 0, 0),
+        exposed=40.0, ra=120.0, dec=-30.0, object_exp=False)
+
 
 def populate_database(telescope_name, instrument_name):
     """
